674dba7e59192534c07fb4a6617c5ff8ed8c40b4
Kshitij Raj <kr91323@gmail.com>
2023-07-05 21:34:14 +0530

    :dog: fixed hero

--- src/components/Information.js
+++ src/components/Information.js
@@ -5,40 +5,39 @@ import Feed from "../assets/3.jpg"
 const Information = () => {
   return (
     <div className="information">
-      <h1>Seva Animal Welfare Charitable trust</h1>
-      <div className="first-sec">
-        <div className="sec-text">
-          <h2>Who are we ?</h2>
-          <p>Here at Seva Animal Welfare Charitable Trust,<br/> 
-            we know that sometimes all it takes to change the world is a little support.<br />
-            Since our founding in 2015  as a Non-Profit organisation, we have been determined to make an impact.<br/> 
-            Beginning with a foundation of 5 people -today the <br />  trust has expanded and  has over 125 active members
-            working in the interest of animal welfare throughout Pune city.
-          </p>
+    <h1>Seva Animal Welfare Charitable trust</h1>
+    <div className="first-sec">
+      <div className="sec-text">
+         <h2>Who are we ?</h2>
+             <p>Here at Seva Animal Welfare Charitable Trust, 
+                we know that sometimes all it takes to change the world is a little support. 
+                Since our founding in 2015 as a Non-Profit organisation, we have been determined to make an impact. 
+                Beginning with a foundation of 5 people -today the trust has expanded and has over 125 active members 
+                working in the interest of animal welfare throughout Pune city.</p>
 
-          <h2> What do we do ?</h2>
-          <p>Our Seva team is mainly focused on ABC drives in various areas of Pune city and <br/>  we successfully sterilized more than 4000 dogs
-            with the help of Canine Control <br/>  And Care and Blue Cross Society Of Pune. <br/>  ( Till Nov 21) and not to forget our volunteers
-            who are dedicated to this noble cause. <br/>  We are always supported by Rescue Charitable Trust, Pets Force Pune and Animal Rescue Trust
-            for all our rescue cases, without whom it is always tough to attend thousands of injured animals.<br/> 
-            Our adoption coordinators are always busy finding loving <br/>  and forever homes for our indies which helps them live a safe and healthy life ahead.
-            Our each volunteer is a feeder himself/herself and plays a key role in our animal welfare. <br/> 
-            We are animal rescuers. Our core belief that animals deserve our love, <br/>  compassion and respect is the basis for everything we do.
-            We believe all our stray friends need a special care and attention. 
-          </p>
+    <h2> What do we do ?</h2>
+    <p>Our Seva team is mainly focused on ABC drives in various areas of Pune city and we successfully sterilized more than 4000 dogs
+         with the help of Canine Control And Care and Blue Cross Society Of Pune. ( Till Nov 21) and not to forget our volunteers 
+         who are dedicated to this noble cause. We are always supported by Rescue Charitable Trust, Pets Force Pune and Animal Rescue Trust 
+         for all our rescue cases, without whom it is always tough to attend thousands of injured animals. 
+         Our adoption coordinators are always busy finding loving and forever homes for our indies which helps them live a safe and healthy life ahead. 
+         Our each volunteer is a feeder himself/herself and plays a key role in our animal welfare. 
+         We are animal rescuers. Our core belief that animals deserve our love, compassion and respect is the basis for everything we do. 
+         We believe all our stray friends need a special care and attention.
+         </p>
+         </div>
+
+         <div className="decor">
+        <img alt="img" src={Feed}/>
         </div>
-
-        <div className="decor">
-          <img alt="img" src={Feed} />
-        </div>
-      </div>
-    </div>
+  </div>
+  </div>
+  
+       
 
 
 
-
-
-
+  
   );
 };
 
