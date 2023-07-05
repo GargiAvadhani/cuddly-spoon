229aae7f6cbded1ec702c4a9662dd23a7c7a60ec
GargiAvadhani <gargiavadhani@gmail.com>
2023-07-05 06:09:50 +0000

    form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -7,6 +7,10 @@ function ContactForm(){
             <h2>Send a message to us!</h2>
             <form>
                 <input placeholder="Name"></input>
+                <input placeholder="Email"></input>
+                <input placeholder="Subject"></input>
+                <textarea placeholder="Message" rows="4"></textarea>
+                <button> Send Message</button>
 
             </form>
         </div>
