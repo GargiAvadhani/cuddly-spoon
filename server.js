48b516c190d70f1faee423cc6097f5d693161e13
Kshitij Raj <kr91323@gmail.com>
2023-07-10 11:29:00 +0530

    :tada: DEV branch

--- server.js
+++ server.js
@@ -1,16 +1,88 @@
 const express = require("express");
-const = express();
-const cors = require("cors");
-const mongoose = require("moongoose")
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
-mongoose.connect("")
-//require route
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3001, function() {
-    console.log("express server os running on port 3001");
+mongoose.connect("mongodb+srv://kr:f5Dcyr64lLpdiTy9@test.tacdw0k.mongodb.net/data", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/src/components/ContactForm.js")
 })
 
+const noteSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        // required: true
+    },
+    email: {
+        type: String,
+        // required: true
+    },
+    subject: {
+        type: String,
+        // required: true
+    },
+    message: {
+        type: String,
+        // required: true
+    }
+})
+
+// const noteSchema ={
+//     name: {
+//       type: String,
+//     //   required: true
+//     },
+//     email: {
+//       type: String,
+//     //   required: true
+//     },
+//     subject: {
+//       type: String,
+//     //   required: true
+//     },
+//     message: {
+//       type: String,
+//     //   required: true
+//     }
+//   };
+  
+const Note = mongoose.model("Note", noteSchema);
+
+app.post("/", async (req, res) => {
+    // const { name, email, subject, message } = req.body;
+
+    try {
+
+        // const user = new Note(req.body);
+        console.log("name: ",req.body);
+        let newNote = new Note({
+            name: req.body.name,
+            email: req.body.email,
+            subject: req.body.subject,
+            message: req.body.message,
+        })
+
+         newNote.save();
+
+        res.json({ message: "Note saved successfully", newNote });
+        // console.log("server.js- ",newNote._id);
+        // await newNote.save();
+
+    } catch (error) {
+        res.json({ error });
+    }
+})
+
+app.listen(5000, function () {
+    console.log("server is running at port 5000");
+})
 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,22 +1,77 @@
-import "./ContactFormStyles.css"
+import React, { useState } from 'react';
+import './ContactFormStyles.css';
 
-function ContactForm(){
-    return(
+function ContactForm() {
+
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Send form data to the backend
+        const response = await fetch("http://localhost:5000/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        });
+
+        const json = await response.json();
+        console.log("form.js- ", json);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value
+        }));
+    };
+
+    return (
         <div className="form-container">
             <h1>Seva Animal Welfare Charitable Trust</h1>
             <h2>Send a message to us!</h2>
-            <form>
-                <input placeholder="Name"></input>
-                <input placeholder="Email"></input>
-                <input placeholder="Subject"></input>
-                <textarea placeholder="Message" rows="4" maxLength="50"></textarea>
-                <button> Send Message</button>
-
+            <form onSubmit={handleSubmit} method='post' action='/'>
+                <input
+                    name="name"
+                    type="text"
+                    placeholder="Name"
+                    value={user.name}
+                    onChange={handleChange}
+                />
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    value={user.email}
+                    onChange={handleChange}
+                />
+                <input
+                    name="subject"
+                    type="text"
+                    placeholder="Subject"
+                    value={user.subject}
+                    onChange={handleChange}
+                />
+                <textarea
+                    name="message"
+                    placeholder="Message"
+                    rows="4"
+                    maxLength="50"
+                    value={user.message}
+                    onChange={handleChange}
+                ></textarea>
+                <button type="submit" >Send Message</button>
             </form>
         </div>
+    );
+}
 
-    )
-        
-    }
-
-    export default ContactForm
+export default ContactForm;
